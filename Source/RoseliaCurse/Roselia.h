1a84105544b054f2051014dc3026021045206ebf
AmilGaul <amilgaulue4@gmail.com>
2022-01-27 15:01:56 +0100

    Character Creation

---
+++ Source/RoseliaCurse/Roselia.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "Roselia.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROSELIACURSE_API ARoselia : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
