b41131bb61f2d10cac5244b1f95b12603fec0913
AmilGaul <amilgaulue4@gmail.com>
2022-01-27 18:43:19 +0100

    Base Logic

--- Source/RoseliaCurse/Roselia.cpp
+++ Source/RoseliaCurse/Roselia.cpp
@@ -2,4 +2,102 @@
 
 
 #include "Roselia.h"
+#include "Engine/EngineTypes.h"
+#include "TimerManager.h"
+#include "PaperSpriteComponent.h"
+#include "PaperSprite.h"
+#include "PaperFlipbookComponent.h"
+#include "PaperFlipbook.h"
+#include "Components/InputComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/CapsuleComponent.h"
 
+
+ARoselia::ARoselia() 
+{
+
+	// Setup Param
+	Speed = 300;
+	Fear = 0;
+	FearMax = 100;
+	SpeedFear = 600;
+
+
+	ConstructorHelpers::FObjectFinder<UPaperSprite>TorchAsset(TEXT("PaperSprite'/Game/Torch.Torch'"));
+	Torch = CreateDefaultSubobject<UPaperSpriteComponent>("TorchSprite");
+	Torch->SetSprite(TorchAsset.Object);
+	
+	ConstructorHelpers::FObjectFinder<UPaperFlipbook>RoseliaAsset(TEXT("PaperFlipbook'/Game/Roselia_FB.Roselia_FB'"));
+	Sprite->SetFlipbook(RoseliaAsset.Object);
+
+	
+	RootComponent = GetCapsuleComponent();	
+	Torch->SetupAttachment(RootComponent);
+	Sprite->SetupAttachment(RootComponent);
+}
+
+void ARoselia::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	MyWorld = GetWorld();
+	CombatStart();
+
+
+}
+
+void ARoselia::FearManagement()
+{
+	if (Fear < FearMax)
+	{
+		Fear++;
+		UE_LOG(LogTemp, Warning, TEXT("%f"), Fear);
+	}
+	else
+	{
+		BerserkMode();
+	}
+
+}
+
+void ARoselia::BerserkMode()
+{
+	MyWorld->GetTimerManager().ClearTimer(FearManagement_TH);
+	UE_LOG(LogTemp, Warning, TEXT("Berserk"));
+
+}
+
+void ARoselia::LightSwitch()
+{
+	if (bIsLightOn) 
+	{
+		bIsLightOn = false;
+		UE_LOG(LogTemp, Warning, TEXT("Light is Off"));
+	}
+
+	else 
+	{
+		bIsLightOn = true;
+		UE_LOG(LogTemp, Warning, TEXT("Light is On"));
+	}
+
+}
+
+void ARoselia::CombatStart()
+{
+	
+	MyWorld->GetTimerManager().SetTimer(FearManagement_TH, this, &ARoselia::FearManagement, 0.1f, true);
+}
+
+void ARoselia::CombatEnd()
+{
+}
+
+void ARoselia::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("LightSwitch", EInputEvent::IE_Pressed, this, &ARoselia::LightSwitch);
+
+}
--- Source/RoseliaCurse/Roselia.h
+++ Source/RoseliaCurse/Roselia.h
@@ -6,12 +6,58 @@
 #include "PaperCharacter.h"
 #include "Roselia.generated.h"
 
-/**
- * 
- */
+UENUM(BlueprintType)
+enum class EAnimDirection :uint8
+{
+	Run_F,
+	Run_B,
+	Run_L,
+	Run_R,
+	Idle,
+};
+
 UCLASS()
 class ROSELIACURSE_API ARoselia : public APaperCharacter
 {
 	GENERATED_BODY()
+	
+	
+
+public:
+	ARoselia();
+
+	virtual void BeginPlay() override;
+
+	float Speed;
+	float SpeedFear;
+	float Fear;
+	float FearMax;
+	bool bIsLightOn;
+	FTimerHandle FearManagement_TH;
+
+	UPROPERTY()
+	class UPaperSpriteComponent* Torch;
+
+	UPROPERTY()
+	class UWorld* MyWorld;
+
+	UFUNCTION()
+	void FearManagement();
+
+	UFUNCTION()
+	void BerserkMode();
+
+	UFUNCTION()
+	void LightSwitch();
+
+	UFUNCTION()
+	void CombatStart();
+
+	UFUNCTION()
+	void CombatEnd();
+
+	UFUNCTION()
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+		
 	
 };
